9a07ac0a86705f0ff8a2b6cdadea05e89595f7d3
Zidane Innis <zidaneinnis@gmail.com>
2023-06-21 20:51:22 -0500

    Starup Setup pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Users />}></Route>
+
+				<Route path="/places/new" element={<NewPlace />}></Route>
+
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
