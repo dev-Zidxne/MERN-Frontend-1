e9c0f1bd1822184d1d7728c4e466874014ec84ac
Zidane Innis <zidaneinnis@gmail.com>
2023-06-23 18:45:30 -0500

    Adding a UserItem Component

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './UsersList.css';
 
-function UserItem() {
-	return <div>UserItem</div>;
+function UserItem(props) {
+	return (
+		<li className="user-item">
+			<div className="user-item__content">
+				<div className="user-item__image">
+					<img src={props.image} alt={props.name}></img>
+				</div>
+				<div className="user-item__info">
+					<h2>{props.name}</h2>
+					<h3>
+						{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+					</h3>
+				</div>
+			</div>
+		</li>
+	);
 }
 
 export default UserItem;
